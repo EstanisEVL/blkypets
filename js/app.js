bdfabcbeb20bd132f505892899d83461bd8796fb
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-07-16 15:00:41 -0300

    Desafio entregable - Interactuar con HTML

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 /*--- Simulador de venta de productos: ----*/
 // Funciones:
-// 1) Solicitar nombre de usuario y saludo:
+// 1) Solicita un nombre de usuario e imprime el saludo personalizado:
 const saludo = usuario => {
     while(true){
         let usuario = prompt("Ingrese su usuario");
         if(usuario != "" && usuario != " " && usuario != null){
             //Saludo
-            alert(`¡Bienvenido ${usuario}!`);
+            let h2 = document.createElement("h2");
+            h2.innerHTML = `<h2>¡Bienvenido ${usuario}!</h2>`;
+            mensaje.append(h2);
             break;
         }else{
             alert("Ingrese un usuario válido");
@@ -14,69 +16,46 @@ const saludo = usuario => {
         }
     }
 }
-// 2) Le muestra al usuario el catálogo de productos y le permite elegir entre buscar productos por nombre y por precio, o filtrar por valor máximo:
-const catalogo = () => {
-    let operador = parseInt(prompt(`\n Elija una operación (Escriba 1, 2 o 3): \n 1. Buscar precio del producto por nombre. \n 2. Buscar nombre del producto por precio. \n 3. ESC.
-    \n Catálogo: \n ${productos[0].nombre} - $${productos[0].precio}\n ${productos[1].nombre} - $${productos[1].precio}\n ${productos[2].nombre} - $${productos[2].precio}\n ${productos[3].nombre} - $${productos[3].precio}\n ${productos[4].nombre} - $${productos[4].precio}`));
-    // Buscar por nombre:
-    if(operador === 1){
-        let nombre = prompt("Elejiste: Buscar precio del producto por nombre. \nIngresá el nombre del producto cuyo precio quieras buscar:").toUpperCase();
-        if(nombre){
-            let productoBuscadoNombre = productos.find(producto => producto.nombre === nombre);
-            alert(`Nombre del producto: ${productoBuscadoNombre.nombre} - Precio del producto: $${productoBuscadoNombre.precio}`);
-        }
-    // Buscar por precio:
-    }else if(operador === 2){
-        let precio = parseInt(prompt("Elejiste: Buscar nombre del producto por precio. \nIngresá el precio del producto cuyo nombre quieras buscar:"));
-        if(!isNaN(precio)){
-            let productoBuscadoPrecio = productos.find(producto => producto.precio === precio);
-            alert(`Precio ingresado: $${productoBuscadoPrecio.precio} - Producto a ese precio: ${productoBuscadoPrecio.nombre}`);
-        }
-    // Salir:
-    }else if(operador === 3){
-        alert("¡Gracias por tu visita!");
-    // Error:
-    }else{
-        alert("Ingrese una operación válida.");
-    }
-}
-// 3) Solicita que el usuario elija dos productos, los agrega al carrito y los devuelve por alert:
+// 2) Le olicita al usuario que elija dos productos y los agrega al carrito:
 const compra = (eleccion) => {
     for(i=0; i<2; i++){
         let eleccion = parseInt(prompt(`Elija un producto para agregar al carrito (1, 2, 3, 4 o 5): 
         \n 1- ${productos[0].nombre} $${productos[0].precio}\n 2- ${productos[1].nombre} $${productos[1].precio}\n 3- ${productos[2].nombre} $${productos[2].precio}\n 4- ${productos[3].nombre} $${productos[3].precio}\n 5- ${productos[4].nombre} $${productos[4].precio}`));
         if(eleccion === 1){
-            carrito.push(new Producto(1, "Collar", 2500));
-            alert(`${productos[0].nombre} agregado al carrito!`);
-            // break;
+            carrito.push(new Producto(1, "COLLAR", 2500));
         }else if(eleccion === 2){
-            carrito.push(new Producto(2, "Correa", 3000));
-            alert(`${productos[1].nombre} agregada al carrito!`);
-            // break;
+            carrito.push(new Producto(2, "CORREA", 3000));
         }else if(eleccion === 3){
-            carrito.push(new Producto(3, "Chapita", 1000));
-            alert(`${productos[2].nombre} agregada al carrito!`);
-            // break;
+            carrito.push(new Producto(3, "CHAPITA", 1000));
         }else if(eleccion === 4){
-            carrito.push(new Producto(4, "Arnés", 5000));
-            alert(`${productos[3].nombre} agregado al carrito!`);
-            // break;
+            carrito.push(new Producto(4, "ARNÉS", 5000));
         }else if(eleccion === 5){
-            carrito.push(new Producto(5, "Pretal", 3500));
-            alert(`${productos[4].nombre} agregado al carrito!`);
-            // break;
+            carrito.push(new Producto(5, "PRETAL", 3500));
         }else{
             alert("Ingrese una opción válida.");
         }
     }
-    // Devuelve los productos agregados al carrito:
-    alert(`Resumen de su compra: \n 1) ${carrito[0].nombre} \n 2) ${carrito[1].nombre}`);
 }
-// 4) Calcula el total de la compra:
+// 3) Imprime los productos que se han agregado al carrito:
+function cargarCarrito(){
+    let compraFinal = document.getElementById("carrito");
+    for(const producto of carrito){
+        let li = document.createElement("li");
+        li.innerHTML = `<p>Producto agregado: </p>
+                        <p>Nombre: ${producto.nombre}</p>
+                        <p>Precio: $${producto.precio}</p>`;
+        compraFinal.append(li);
+    }
+}
+// 4) Calcula el total de la compra y lo muestra debajo del carrito:
+let divTotal = document.getElementById("total");
 const calculoTotal = () =>{
     const total = carrito.reduce((acumulador, elemento) => acumulador  + elemento.precio, 0);
-    alert(`Total de su compra: $${total}`);
+    let mostrarTotal = document.createElement("h2");
+    mostrarTotal.innerHTML = `<p> Total de su compra: $${total}</p>`;
+    divTotal.append(mostrarTotal);
 }
+
 // Clases, arreglos y variables:
 class Producto{
     constructor (id, nombre, precio, cantidad){
@@ -91,18 +70,33 @@ class Producto{
     }
 }
 const productos = [
-    {id: 1, nombre: "COLLAR", precio: 2500, cantidad: 45, vendido: false},
-    {id: 2, nombre: "CORREA", precio: 3000, cantidad: 40, vendido: false},
-    {id: 3, nombre: "CHAPITA", precio: 1000, cantidad: 150, vendido: false},
-    {id: 4, nombre: "ARNES", precio: 5000, cantidad: 15, vendido: false},
-    {id: 5, nombre: "PRETAL", precio: 3500, cantidad: 30, vendido: false},
+    {id: 1, nombre: "COLLAR", precio: 2500, cantidad: 45, vendido: false, img: 'images/Collares.jpg'},
+    {id: 2, nombre: "CORREA", precio: 3000, cantidad: 40, vendido: false, img: "images/Correas.jpg"},
+    {id: 3, nombre: "CHAPITA", precio: 1000, cantidad: 150, vendido: false, img: "images/DestChapitas.jpg"},
+    {id: 4, nombre: "ARNES", precio: 5000, cantidad: 15, vendido: false, img: "images/Arneses.jpg"},
+    {id: 5, nombre: "PRETAL", precio: 3500, cantidad: 30, vendido: false, img: "images/Pretales.jpg"},
 ];
 const carrito = [];
+
+let contenedor = document.getElementById("contenedor");
+
+for(const producto of productos){
+    let li = document.createElement("li");
+    li.innerHTML = `<p> ID: ${producto.id}</p>
+                    <p> Nombre: ${producto.nombre}</p>
+                    <p> Precio: $${producto.precio}</p>
+                    <p> En stock: ${producto.cantidad} unidades.</p>
+                    <img class= "img-fluid w-25" src= "${producto.img}">
+    `;
+    contenedor.append(li);
+}
+
 // Código:
 saludo();
-catalogo();
 compra();
+cargarCarrito();
 calculoTotal();
+
 
 
 
