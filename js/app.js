541a7f077d071b19b5d04fce34e7f8988f1feeab
Estanislao Elias Varela Lucius <estanislaovl@gmail.com>
2022-07-21 12:41:58 -0300

    Segundo commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*--- Simulador de venta de productos: ----*/
 import {productos} from "./stock.js";
-import {carritoIndex} from "./carritoIndex.js";
+// import {carritoIndex} from "./carritoIndex.js";
 
 // Funciones:
 // 1) Se ingresa el nombre de usuario en el formulario e imprime el saludo en base al usuario ingresado:
@@ -8,7 +8,7 @@ function saludo(e){
     e.preventDefault();
     // Crea el saludo personalizado:
     let saludo = document.createElement("h2");
-    saludo.innerHTML = `<h2>¡Bienvenido <span>${nombre.value}</span>!</h2>`;
+    saludo.innerHTML = `<h2>¡Bienvenid@ <span>${nombre.value}</span>!</h2>`;
     saludo.className = "saludo";
     principal.prepend(saludo);
     // Borra el formulario:
@@ -16,100 +16,93 @@ function saludo(e){
 }
 
 // 2) Muestra productos y permite que el usuario los agregue al carrito:
-const compra = (productos) => {
-    const contenedorProductos = document.getElementById("contenedor-productos");
-    productos.forEach(producto => {
-        const div = document.createElement("div");
-        div.classList.add("tarjeta");
-        div.innerHTML += `
-                        <div class="tarjeta" style="width: 18rem">
-                            <img src='${producto.img}' class="img-fluid card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${producto.nombre}</h5>
-                                <p class="card-text">Precio: $${producto.precio}</p>
-                                <button class="btn btn-dark" id=boton${producto.id}>COMPRAR</button>
-                            </div>
+const contenedorProductos = document.getElementById("contenedor-productos");
+const contenedorCarrito = document.getElementById("carrito-contenedor");
+const vaciarCarrito = document.getElementById("vaciar-carrito");
+const contadorCarrito = document.getElementById("contador-carrito");
+const precioTotal = document.getElementById("precio-total");
+
+let carrito = [];
+// Vaciar carrito:
+vaciarCarrito.addEventListener("click", () => {
+    carrito.length = 0;
+    actualizarCarrito();
+})
+// Renderiza los productos:
+productos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("tarjeta");
+    div.innerHTML = `
+                    <div class="tarjeta" style="width: 18rem">
+                        <img src='${producto.img}' class="img-fluid card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">Precio: $${producto.precio}</p>
+                        <button class="btn btn-dark" id="agregar${producto.id}">COMPRAR</button>
                         </div>
-                        `;
-        contenedorProductos.prepend(div);
+                    </div>
+                    `
+    contenedorProductos.prepend(div);
 
-        const boton = document.getElementById(`boton${producto.id}`);
+    const boton = document.getElementById(`agregar${producto.id}`);
+    boton.addEventListener("click", () => {
+        agregarAlCarrito(producto.id);
+        alert(`¡${producto.nombre} agregado al carrito!`);
+    });
+})
+// Agregar productos al carrito:
+const agregarAlCarrito = (productoId) =>{
+    const existe = carrito.some(producto => producto.id === productoId);
 
-        boton.addEventListener('click', () => {
-            carritoIndex(producto.id);
-            alert(`¡${producto.nombre} agregado al carrito!`);
-            let contadorProducto = getElementById("contador-carrito");
-            contadorProducto.innerHTML = `<p>0+${producto.cantidad}</p>`;
+    if(existe){
+        const producto = carrito.map(producto => {
+            if(producto.id === productoId){
+                producto.cantidad++;
+            }
+        })
+    }else{
+        const producto = productos.find((producto) => producto.id === productoId);
+        carrito.push(producto);
+    }
+    actualizarCarrito();
+}
+// Eliminar productos del carrito:
+const eliminarDelCarrito = (productoId) => {
+    const producto = carrito.find((producto) => producto.id === productoId);
+    const indice = carrito.indexOf(producto);
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+// Renderiza el carrito:
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = "";
+
+    carrito.forEach((producto) => {
+        const div = document.createElement("div");
+        div.className = ("producto-carrito");
+        div.innerHTML = `<p>${producto.nombre}</p>
+                        <p>Precio: $${producto.precio}</p>
+                        <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+                        <img src='${producto.img}' class="img-fluid card-img-top w-25" alt="...">
+                        <button id="eliminar(${producto.id})" class="boton-eliminar"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button>
+                        `
+        contenedorCarrito.prepend(div);
+
+        const boton = document.getElementById(`eliminar(${producto.id})`);
+        boton.addEventListener("click", () => {
+            eliminarDelCarrito(producto.id);
         });
     });
+    contadorCarrito.innerText = carrito.length;
+    precioTotal.innerText = carrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
 }
 
-
-// const compra = (eleccion) => {
-//     for(i=0; i<2; i++){
-//         let eleccion = parseInt(prompt(`Elija un producto para agregar al carrito (1, 2, 3, 4 o 5): 
-//         \n 1- ${productos[0].nombre} $${productos[0].precio}\n 2- ${productos[1].nombre} $${productos[1].precio}\n 3- ${productos[2].nombre} $${productos[2].precio}\n 4- ${productos[3].nombre} $${productos[3].precio}\n 5- ${productos[4].nombre} $${productos[4].precio}`));
-//         if(eleccion === 1){
-//             carrito.push(new Producto(1, "COLLAR", 2500));
-//         }else if(eleccion === 2){
-//             carrito.push(new Producto(2, "CORREA", 3000));
-//         }else if(eleccion === 3){
-//             carrito.push(new Producto(3, "CHAPITA", 1000));
-//         }else if(eleccion === 4){
-//             carrito.push(new Producto(4, "ARNÉS", 5000));
-//         }else if(eleccion === 5){
-//             carrito.push(new Producto(5, "PRETAL", 3500));
-//         }else{
-//             alert("Ingrese una opción válida.");
-//         }
-//     }
-// }
-
-// 3) Imprime los productos que se han agregado al carrito:
-// function cargarCarrito(){
-//     let compraFinal = document.getElementById("carrito");
-//     for(const producto of carrito){
-//         let li = document.createElement("li");
-//         li.innerHTML = `<p>Producto agregado: </p>
-//                         <p>Nombre: ${producto.nombre}</p>
-//                         <p>Precio: $${producto.precio}</p>`;
-//         compraFinal.append(li);
-//     }
-// }
-
-// 4) Calcula el total de la compra y lo muestra debajo del carrito:
-// let divTotal = document.getElementById("total");
-// const calculoTotal = () =>{
-//     const total = carrito.reduce((acumulador, elemento) => acumulador  + elemento.precio, 0);
-//     let mostrarTotal = document.createElement("h2");
-//     mostrarTotal.innerHTML = `<p> Total de su compra: $${total}</p>`;
-//     divTotal.append(mostrarTotal);
-// }
-
-// Clases, arreglos y variables:
 let ingreso = document.getElementById("ingreso");
 let nombre = document.getElementById("nombre");
 let formulario = document.getElementById("formulario");
 let principal = document.getElementById("principal");
 
 formulario.addEventListener("submit", saludo);
-// let contenedor = document.getElementById("contenedor");
-
-// for(const producto of productos){
-//     let li = document.createElement("li");
-//     li.innerHTML = `<p> ID: ${producto.id}</p>
-//                     <p> Nombre: ${producto.nombre}</p>
-//                     <p> Precio: $${producto.precio}</p>
-//                     <p> En stock: ${producto.cantidad} unidades.</p>
-//                     <img class= "img-fluid w-25" src= "${producto.img}">
-//     `;
-//     contenedor.append(li);
-// }
-
-// Código:
-// saludo();
-compra(productos);
-// calculoTotal();
 
 
 
